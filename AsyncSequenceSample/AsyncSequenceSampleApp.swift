df65bc3376a3e3060da50ac32604735dd3a4791b
Nico Ameghino <nicolas.ameghino@gmail.com>
2026-01-30 14:11:09 -0300

    Initial Commit

---
+++ AsyncSequenceSample/AsyncSequenceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSequenceSampleApp.swift
+//  AsyncSequenceSample
+//
+//  Created by Nicolas Ameghino on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSequenceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
